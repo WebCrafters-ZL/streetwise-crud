1a2be9306c74d043cd301c39a108c9577ccafdd8
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2023-11-22 16:05:08 -0300

    feat: Demais operações

--- src/App.java
+++ src/App.java
@@ -14,7 +14,7 @@ public class App {
     public static void cadastrar() {
         // Código para cadastrar uma tatuagem
         Tatuagem t = new Tatuagem();
-        System.out.println("**\tCadastrar tatuagem**");
+        System.out.println("\t**Cadastrar tatuagem**");
         System.out.println("\tInsira as informações da tatuagem.");
         System.out.println("\t\tDigite o nome da tatuagem:");
         t.nome = input.nextLine();
@@ -26,20 +26,71 @@ public class App {
         System.out.println("Tatuagem cadastrada com sucesso.");
     }
 
+    public static void pesquisar() {
+        // Código para pesquisar uma tatuagem
+        System.out.println("\t**Pesquisar tatuagem**");
+        System.out.println("\t\tDigite o nome da tatuagem:");
+        String nomeTatuagem = input.nextLine();
+        for (int i = 0; i < indice; i++) {
+            Tatuagem t = catalogo[i];
+            if (t != null && t.nome.toLowerCase().contains(nomeTatuagem.toLowerCase())) {
+                System.out.print("Tatuagem encontrada:\t");
+                System.out.println("Cód.: " + t.id + " | Nome: " + t.nome + " | Estilo: " + t.estilo);
+                break;
+            }
+        }
+    }
+
     public static void alterar() {
         // Código para alterar uma tatuagem
+        System.out.println("\t**Alterar tatuagem**");
+        System.out.println("\tPesquise a tatuagem que deseja alterar");
+        System.out.println("\t\tDigite o nome da tatuagem:");
+        String nomeTatuagem = input.nextLine();
+        for (int i = 0; i < indice; i++) {
+            Tatuagem t = catalogo[i];
+            if (t != null && t.nome.toLowerCase().contains(nomeTatuagem.toLowerCase())) {
+                System.out.print("Tatuagem encontrada:\t");
+                System.out.println("Cód.: " + t.id + " | Nome: " + t.nome + " | Estilo: " + t.estilo);
+                System.out.println("Digite o nome atualizado:");
+                t.nome = input.nextLine();
+                System.out.println("Digite o estilo atualizado:");
+                t.estilo = input.nextLine();
+                catalogo[i] = t;
+                System.out.println("Tatuagem atualizada com sucesso.");
+                System.out.println("Cód.: " + t.id + " | Nome: " + t.nome + " | Estilo: " + t.estilo);
+                break;
+            }
+        }
     }
 
     public static void excluir() {
         // Código para excluir uma tatuagem
-    }
-
-    public static void pesquisar() {
-        // Código para pesquisar uma tatuagem
+        System.out.println("\t**Excluir tatuagem**");
+        System.out.println("\tPesquise a tatuagem que deseja excluir");
+        System.out.println("\t\tDigite o nome da tatuagem:");
+        String nomeTatuagem = input.nextLine();
+        for (int i = 0; i < indice; i++) {
+            Tatuagem t = catalogo[i];
+            if (t != null && t.nome.toLowerCase().contains(nomeTatuagem.toLowerCase())) {
+                System.out.print("Tatuagem encontrada:\t");
+                System.out.println("Cód.: " + t.id + " | Nome: " + t.nome + " | Estilo: " + t.estilo);
+                catalogo[i] = null;
+                System.out.println("Tatuagem excluída com sucesso.");
+                break;
+            }
+        }
     }
 
     public static void listar() {
         // Código para listar todas as tatuagens
+        System.out.println("\t**Listar tatuagens**");
+        for (int i = 0; i < indice; i++) {
+            Tatuagem t = catalogo[i];
+            if (t != null) {
+                System.out.println("Cód.: " + t.id + "\t|\tNome: " + t.nome + "\t|\tEstilo: " + t.estilo);
+            }
+        }
     }
 
     public static void sair() {
@@ -54,10 +105,10 @@ public class App {
             System.out.println("\t*** Catálogo de Tatuagens *** \t");
             System.out.println("\tOpções do menu:");
             System.out.println("\t\t(C)adastrar");
+            System.out.println("\t\t(P)esquisar");
             System.out.println("\t\t(A)lterar");
             System.out.println("\t\t(E)xcluir");
-            System.out.println("\t\t(P)esquisar");
-            System.out.println("\t\t(L)istar");
+            System.out.println("\t\t(L)istar tudo");
             System.out.println("\t\t(S)air");
             System.out.println("\tDigite a primeira letra da opção desejada:");
             char opcao = input.nextLine().toUpperCase().charAt(0);
@@ -65,14 +116,14 @@ public class App {
                 case 'C':
                     cadastrar();
                     break;
+                case 'P':
+                    pesquisar();
+                    break;
                 case 'A':
                     alterar();
                     break;
                 case 'E':
                     excluir();
-                    break;
-                case 'P':
-                    pesquisar();
                     break;
                 case 'L':
                     listar();
@@ -84,9 +135,6 @@ public class App {
                     System.out.println("ERRO! Selecione uma opção da lista.");
                     break;
             }
-
         }
-        ;
     }
-
 }
